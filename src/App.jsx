59fa19c3e700caafad663740b2eccc0dd06965fe
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-09 14:00:29 +0300

    add react-router-dom and routes on pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
+import routes from "./routes";
 
 function App() {
-  
   return (
-    <div className="App" >
-      
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+          <Link to="/About">About</Link>
+          </li>
+          <li>
+          <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+          <Link to="/skills">Skills</Link>
+          </li>
+          <li>
+          <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        {routes.map(({ path, component: Component }, index) => (
+          <Route key={index} path={path} element={<Component />} />
+        ))}
+      </Routes>
     </div>
   );
 }
