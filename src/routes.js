618878e3c7ff5e4bd9d039bf3a0b5536d62b79cc
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-09 13:33:39 +0300

    added folder structure

---
+++ src/routes.js
@@ -0,0 +1,30 @@
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Portfolio from "./pages/Portfolio"
+import Skills from "./pages/Skills"
+import Contact from "./pages/Contact"
+
+const routes = [
+    {
+        path: "/",
+        component: Home,
+    },
+    {
+        path: "/About",
+        component: About,
+    },
+    {
+        path: "/Portfolio",
+        component: Portfolio,
+    },
+    {
+        path: "/Skills",
+        component: Skills,
+    },
+    {
+        path: "/Contact",
+        component: Contact,
+    },
+]
+
+export default routes;
