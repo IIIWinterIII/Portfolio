42e304aafa4e37d84d7ae059d162e58137279b23
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-10 16:42:18 +0300

    done 50% on footer, starting work on home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import "../styles/components/Footer.css"
+import React from "react";
+import "../styles/components/Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTelegram, faGithub } from "@fortawesome/free-brands-svg-icons";
 
-
-function Footer(props) {
-    return (
-        <div>
-            <h1>Footer</h1>
-        </div>
-    );
+function Footer() {
+  return (
+    <footer className="container-footer">
+      <h3>Разработано и создано IIIWinteRIII</h3>
+      <h3>Авторские права © 2024 N.K.</h3>
+      <div className="contactBox">
+          <a href="mailto:nizamikas@gmail.com">nizamikas@gmail.com</a>
+          <a href="https://t.me/iwinter1">
+            <FontAwesomeIcon icon={faTelegram} className="icon"/>
+          </a>
+          <a href="https://github.com/IIIWinterIII">
+            <FontAwesomeIcon icon={faGithub} className="icon"/>
+          </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import "../styles/pages/Home.css"
 
-function Home(props) {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+function Home() {
+  return (
+    <main>
+      <div>
+        <h1>Привет! Меня зовут Низами.</h1>
+        <p>
+          Frontend-разработчик, создаю современные и удобные веб-приложения.
+        </p>
+      </div>
+      <div>
+        <h2>Технологии:</h2>
+        <ul>
+          <li>React</li>
+          <li>JavaScript (ES6+)</li>
+          <li>HTML5 и CSS3</li>
+          <li>Git и GitHub</li>
+        </ul>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
