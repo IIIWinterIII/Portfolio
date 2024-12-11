1a91527ec00ae15ae4595f22648f1bbff7ba48ee
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-11 15:59:59 +0300

    add element in html

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import "../styles/pages/Projects.css";
 
-function Projects(props) {
-    return (
-        <div>
-            <h1>Projects</h1>
+function Projects() {
+  return (
+    <main className="container-projects">
+      <section className="text-section">
+        <h1>Мои последние работы</h1>
+        <h3>
+          Вот несколько проектов, которые я создавал для обучения и практики.
+        </h3>
+      </section>
+      <div className="box-with-projects">
+        <div className="project">
+          <p>image</p>
+          <div className="description">описание</div>
+          <a href="">GitHub</a>
+          <a href="">Demo</a>
         </div>
-    );
+      </div>
+    </main>
+  );
 }
 
 export default Projects;
