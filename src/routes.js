76abd6ed9d4ef6cdfacfc370006f0e6f10e1301c
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-12 14:20:41 +0300

    remove Resume page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Projects from "./pages/Projects"
-import Resume from "./pages/Resume"
 import Contact from "./pages/Contact"
 
-import { faHome, faUser, faDisplay, faFileLines, faAddressCard } from "@fortawesome/free-solid-svg-icons"
+import { faHome, faUser, faDisplay, faAddressCard } from "@fortawesome/free-solid-svg-icons"
 
 const routes = [
     {
@@ -25,12 +24,6 @@ const routes = [
         component: Projects,
         name: "Projects",
         icons: faDisplay,
-    },
-    {
-        path: "/resume",
-        component: Resume,
-        name: "Resume",
-        icons: faFileLines,
     },
     {
         path: "/contact",
