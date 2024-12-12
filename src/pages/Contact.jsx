0f70079f1ff6ee3c0c7ff913211b8c0e5514c7dd
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-12 16:08:22 +0300

    created a contact page and styles for it

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React from "react";
+import "../styles/pages/Contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTelegram, faGithub } from "@fortawesome/free-brands-svg-icons";
 
-function Contact(props) {
-    return (
-        <div>
-            <h1>Contact</h1>
+function Contact() {
+  return (
+    <main className="container-contact">
+      <div className="contacts-box">
+        <div className="contact-messager">
+          <h1>Google Mail</h1>
+          <h1>-</h1>
+          <a href="mailto:nizamikas@gmail.com" className="mail">
+            nizamikas@gmail.com
+          </a>
         </div>
-    );
+        <div className="contact-messager">
+          <h1>Telegram</h1>
+          <h1>-</h1>
+          <a href="https://t.me/iwinter1">
+            <FontAwesomeIcon icon={faTelegram} className="icon" />
+          </a>
+        </div>
+        <div className="contact-messager">
+          <h1>GitHub</h1>
+          <h1>-</h1>
+          <a href="https://github.com/IIIWinterIII">
+            <FontAwesomeIcon icon={faGithub} className="icon" />
+          </a>
+        </div>
+        <div className="contact-messager">
+          <h1>Resume hh</h1>
+          <h1>-</h1>
+          <a
+            href="..\assets\resume\resume.pdf"
+            download="resume.pdf"
+            class="download-resume-button"
+          >
+            Download Resume PDF
+          </a>
+        </div>
+      </div>
+
+      <div className="advertising-text">
+        <div className="create-site">
+        <p>Если вам <span>нужен</span> современный <span>сайт</span>, я готов помочь с:</p>
+        <ul>
+          <li>
+          <span>Одностраничниками</span> (сайты-визитки, портфолио и т.д. — отлично
+            подходят для быстрого ознакомления с вашим бизнесом и предоставления
+            услуг);
+          </li>
+          <li>
+          <span>Лендингами</span> (идеально для продвижения товаров и услуг, создания
+            целевых страниц для рекламных кампаний и увеличения конверсий);
+          </li>
+          <li>
+          <span>Прогрессивными веб-приложениями (PWA)</span> с расширенной
+            функциональностью ( идеально для интерактивного взаимодействия с
+            пользователями и повышения удобства работы с вашим сервисом).
+          </li>
+        </ul>
+        </div>
+        
+        <p>
+          Также я <span>открыт к предложениям</span> о сотрудничестве в роли
+          <span> фронтенд-разработчика</span> или <span>HTML-верстальщика</span>.
+        </p>
+        <p>Буду рад обсудить ваш <span>проект</span> или ответить на <span>вопросы</span>!</p>
+      </div>
+    </main>
+  );
 }
 
 export default Contact;
