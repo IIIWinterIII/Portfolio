f637f2c93a40457b423d0f531de5115f31fa48fb
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-14 15:07:20 +0300

    page always on top

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./styles/App.css";
 import { Route, Routes } from "react-router-dom";
 import routes from "./routes";
+import PageAlwaysOnTop from "./components/PageAlwaysOnTop.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import ScrollToTopButton from "./components/ScrollToTopButton.jsx";
@@ -10,12 +11,13 @@ function App() {
     <div className="app-container">
       <Header />
       <main className="content">
+        <PageAlwaysOnTop />
         <Routes>
           {routes.map(({ path, component: Component }, index) => (
             <Route key={index} path={path} element={<Component />} />
           ))}
         </Routes>
-          <ScrollToTopButton />
+        <ScrollToTopButton />
       </main>
       <Footer />
     </div>
