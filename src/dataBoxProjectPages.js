a3a70de020b650f8335ba8bbe6d518b2ec9d1bb0
IIIWinterIII <nikitavakulin39@gmail.com>
2025-01-05 21:11:41 +0300

    removal Beauty-salon from projects

--- src/dataBoxProjectPages.js
+++ src/dataBoxProjectPages.js
@@ -3,9 +3,42 @@ const dataBox = [
     img: "assets/images/Web-Development-Services.png",
     alt: "Web-Development-Services",
     title: "Web-Development-Services",
-    subtitle: "Этот проект представляет услуги по разработке сайтов, включая различные типы веб-ресурсов: от лендингов до интернет-магазинов. Простая форма заявки позволяет быстро начать сотрудничество и получить качественный результат.",
+    subtitle:
+      "Этот проект представляет услуги по разработке сайтов, включая различные типы веб-ресурсов: от лендингов до интернет-магазинов. Простая форма заявки позволяет быстро начать сотрудничество и получить качественный результат.",
     repoUrl: "https://github.com/IIIWinterIII/Dev-services-website",
     demoUrl: "https://iiiwinteriii.github.io/Dev-services-website/",
+  },
+
+  // {
+  //   img: "assets/images/Beauty-salon.png",
+  //   alt: "beauty-salon",
+  //   title: "Beauty-salon",
+  //   subtitle:
+  //     "Сайт — шаблон. Он предоставляет удобную платформу для ознакомления с услугами и ценами, а также включает галерею работ мастеров. Адаптивный дизайн, что позволяет клиентам легко находить нужную информацию в любое время и в любом месте.",
+  //   repoUrl: "https://github.com/IIIWinterIII/Beauty-salon",
+  //   demoUrl: "https://iiiwinteriii.github.io/Beauty-salon/",
+  // },
+  {
+    img: "assets/images/Muhammad-Ali.png",
+    alt: "Muhammad-Ali",
+    title: "Muhammad-Ali",
+    subtitle:
+      "Одностраничный сайт, посвящённый Мухамеду Али, с краткой информацией о его достижениях, ярким моментом на фото и вдохновляющей цитатой. Я использовал стильный и минималистичный дизайн, чтобы сделать информацию лёгкой для восприятия.",
+    subtitle2:
+      "На основе этого макета я могу создать сайт для любых нужд: конспектов лекций, учебных заметок, или справочников, делая информацию доступной и понятной.",
+    repoUrl: "https://github.com/IIIWinterIII/WIki-Muhammad-Ali",
+    demoUrl: "https://iiiwinteriii.github.io/WIki-Muhammad-Ali/",
+  },
+  {
+    img: "assets/images/Random-Quote-Machine.png",
+    alt: "Random-Quote-Machine",
+    title: "Random-Quote-Machine",
+    subtitle:
+      "Развлекательное приложение 'Машина цитат' с минималистичным дизайном. В нём собраны вдохновляющие и мудрые цитаты от великих людей, которые могут поднять настроение или стать источником мотивации.",
+    subtitle2:
+      "Можно адаптировать этот проект для создания карточек для изучения языков, что сделает его полезным инструментом для обучения. Это идеальный проект для создания простого, но функционального ресурса, который всегда будет интересен пользователям.",
+    repoUrl: "https://github.com/IIIWinterIII/Random-Quote-Machine",
+    demoUrl: "https://iiiwinteriii.github.io/Random-Quote-Machine/",
   },
   {
     img: "assets/images/Documentation-Page.jpg",
@@ -20,38 +53,13 @@ const dataBox = [
     demoUrl: "https://iiiwinteriii.github.io/Documentation-Page/",
   },
   {
-    img: "assets/images/Beauty-salon.png",
-    alt: "beauty-salon",
-    title: "Beauty-salon",
-    subtitle:
-      "Сайт — шаблон. Он предоставляет удобную платформу для ознакомления с услугами и ценами, а также включает галерею работ мастеров. Адаптивный дизайн, что позволяет клиентам легко находить нужную информацию в любое время и в любом месте.",
-    repoUrl: "https://github.com/IIIWinterIII/Beauty-salon",
-    demoUrl: "https://iiiwinteriii.github.io/Beauty-salon/",
-  },
-  {
-    img: "assets/images/Muhammad-Ali.png",
-    alt: "Muhammad-Ali",
-    title: "Muhammad-Ali",
-    subtitle: "Одностраничный сайт, посвящённый Мухамеду Али, с краткой информацией о его достижениях, ярким моментом на фото и вдохновляющей цитатой. Я использовал стильный и минималистичный дизайн, чтобы сделать информацию лёгкой для восприятия.",
-    subtitle2: "На основе этого макета я могу создать сайт для любых нужд: конспектов лекций, учебных заметок, или справочников, делая информацию доступной и понятной.",
-    repoUrl: "https://github.com/IIIWinterIII/WIki-Muhammad-Ali",
-    demoUrl: "https://iiiwinteriii.github.io/WIki-Muhammad-Ali/",
-  },
-  {
-    img: "assets/images/Random-Quote-Machine.png",
-    alt: "Random-Quote-Machine",
-    title: "Random-Quote-Machine",
-    subtitle: "Развлекательное приложение 'Машина цитат' с минималистичным дизайном. В нём собраны вдохновляющие и мудрые цитаты от великих людей, которые могут поднять настроение или стать источником мотивации.",
-    subtitle2: "Можно адаптировать этот проект для создания карточек для изучения языков, что сделает его полезным инструментом для обучения. Это идеальный проект для создания простого, но функционального ресурса, который всегда будет интересен пользователям.",
-    repoUrl: "https://github.com/IIIWinterIII/Random-Quote-Machine",
-    demoUrl: "https://iiiwinteriii.github.io/Random-Quote-Machine/",
-  },
-  {
     img: "assets/images/Markdown-Previewer.png",
     alt: "Markdown-Previewer",
     title: "Markdown-Previewer",
-    subtitle: "Проект представляет собой две доски, одна из которых предназначена для ввода текста в формате Markdown, а вторая — для отображения того, как будет выглядеть результат. Простой и интуитивно понятный интерфейс. Идеально подходит для изучения и практики работы с Markdown языком разметки.",
-    subtitle2: "Проект является отличным инструментом для тех, кто хочет улучшить свои навыки работы с Markdown.",
+    subtitle:
+      "Проект представляет собой две доски, одна из которых предназначена для ввода текста в формате Markdown, а вторая — для отображения того, как будет выглядеть результат. Простой и интуитивно понятный интерфейс. Идеально подходит для изучения и практики работы с Markdown языком разметки.",
+    subtitle2:
+      "Проект является отличным инструментом для тех, кто хочет улучшить свои навыки работы с Markdown.",
     repoUrl: "https://github.com/IIIWinterIII/Markdown-Previewer",
     demoUrl: "https://iiiwinteriii.github.io/Markdown-Previewer/",
   },
